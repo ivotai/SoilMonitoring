bf0ba6156ec49b88147cb1c30e497739cc874c3f
ivotai <ivotai@163.com>
2023-08-07 19:22:42 +0800

    8-7 结束。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 android {
+
     namespace = "com.unicorn.soilmonitoring"
     compileSdk = 33
 
@@ -14,7 +15,16 @@ android {
         versionCode = 1
         versionName = "1.0"
         ndk {
-            abiFilters += listOf("armeabi", "armeabi-v7a", "arm64-v8a")
+            abiFilters += listOf("armeabi", "armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+        }
+    }
+
+    signingConfigs {
+        create("unicorn") {
+            storeFile = file("C:\\Users\\13276\\AndroidStudioProjects\\SoilMonitoring\\unicorn.jks")
+            storePassword = "unicorn"
+            keyAlias = "unicorn"
+            keyPassword = "unicorn"
         }
     }
 
@@ -24,14 +34,17 @@ android {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("unicorn")
         }
         debug {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("unicorn")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -54,8 +67,9 @@ dependencies {
     implementation("com.github.DylanCaiCoding.ViewBindingKTX:viewbinding-ktx:2.1.0")
     implementation("com.github.DylanCaiCoding.ViewBindingKTX:viewbinding-base:2.1.0")
 
-    // BaiduMap
-    implementation("com.baidu.lbsyun:BaiduMapSDK_Map:7.5.4")
+    // BaiduMapSDK
+    implementation("com.baidu.lbsyun:BaiduMapSDK_Map:7.5.8")
+    implementation("com.baidu.lbsyun:BaiduMapSDK_Search:7.5.8")
 
 }
 
--- app/src/main/java/com/unicorn/soilmonitoring/MainActivity.kt
+++ app/src/main/java/com/unicorn/soilmonitoring/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.unicorn.soilmonitoring
 
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener
+import com.baidu.mapapi.search.poi.PoiCitySearchOption
+import com.baidu.mapapi.search.poi.PoiDetailResult
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult
+import com.baidu.mapapi.search.poi.PoiIndoorResult
+import com.baidu.mapapi.search.poi.PoiResult
+import com.baidu.mapapi.search.poi.PoiSearch
+import com.baidu.mapapi.search.weather.WeatherDataType
+import com.baidu.mapapi.search.weather.WeatherSearch
+import com.baidu.mapapi.search.weather.WeatherSearchOption
 import com.unicorn.soilmonitoring.databinding.ActivityMainBinding
 import com.unicorn.soilmonitoring.ui.base.BaseAct
 
@@ -7,6 +17,56 @@ import com.unicorn.soilmonitoring.ui.base.BaseAct
 class MainActivity : BaseAct<ActivityMainBinding>() {
 
     override fun initViews() {
+        binding.tvSearch.setOnClickListener {
+            search()
+        }
+    }
+
+    private fun search() {
+
+        val districtID = "110105" // 天安门区域ID
+
+        val weatherSearchOption = WeatherSearchOption()
+            .weatherDataType(WeatherDataType.WEATHER_DATA_TYPE_ALL)
+            .districtID(districtID)
+
+        val mWeatherSearch = WeatherSearch.newInstance()
+        mWeatherSearch.setWeatherSearchResultListener {
+            it
+            it.realTimeWeather
+            val realTimeWeather = it.realTimeWeather
+            realTimeWeather.temperature
+
+        }
+
+        mWeatherSearch.request(weatherSearchOption);
+    }
+
+    private fun search2() {
+        val p = PoiSearch.newInstance();
+        val listener: OnGetPoiSearchResultListener = object : OnGetPoiSearchResultListener {
+            override fun onGetPoiResult(poiResult: PoiResult) {
+                poiResult
+            }
+
+            override fun onGetPoiDetailResult(poiDetailSearchResult: PoiDetailSearchResult) {
+                poiDetailSearchResult
+            }
+
+            override fun onGetPoiIndoorResult(poiIndoorResult: PoiIndoorResult) {
+
+            }
+
+            //废弃
+            override fun onGetPoiDetailResult(poiDetailResult: PoiDetailResult) {}
+        }
+        p.setOnGetPoiSearchResultListener(listener)
+        p.searchInCity(
+            PoiCitySearchOption()
+                .city("北京") //必填
+                .keyword("美食") //必填
+                .pageNum(0)
+        )
     }
 
     override fun onPause() {
