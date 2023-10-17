5e6cc085e56455784d925b1564122da474d2794d
ivotai <ivotai@163.com>
2023-10-17 11:19:42 +0800

    .

--- app/src/main/java/com/unicorn/soilmonitoring/ui/fra/CollectListFra2.kt
+++ app/src/main/java/com/unicorn/soilmonitoring/ui/fra/CollectListFra2.kt
@@ -1,14 +1,14 @@
 package com.unicorn.soilmonitoring.ui.fra
 
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.list.listItems
 import com.drake.brv.annotaion.DividerOrientation
 import com.drake.brv.utils.divider
 import com.drake.brv.utils.grid
-import com.drake.brv.utils.linear
 import com.drake.brv.utils.setup
 import com.drake.statusbar.statusPadding
 import com.unicorn.soilmonitoring.R
 import com.unicorn.soilmonitoring.databinding.FraCollectList2Binding
-import com.unicorn.soilmonitoring.databinding.FraCollectListBinding
 import com.unicorn.soilmonitoring.model.Collect
 import com.unicorn.soilmonitoring.ui.base.BaseFra
 import splitties.resources.color
@@ -34,6 +34,13 @@ class CollectListFra2 : BaseFra<FraCollectList2Binding>() {
 
                 onBind {
 
+                }
+
+                onClick(R.id.itvOperation){
+                    val items = listOf("已送样","完成检测","已出报告","退回重采")
+                    MaterialDialog(requireContext()).show {
+                        listItems(items = items)
+                    }
                 }
             }.models = (1..20).toList().map { Collect() }
         }
